4e2e5f5df97f1c6eae474ccb010120db2bc35c60
valderane <valderane@yahoo.fr>
2018-02-26 01:39:56 +0100

    mise en place de la translation et de la generation des canons

--- canons.js
+++ canons.js
@@ -4,11 +4,22 @@ var ecran_height = window.innerHeight;
 var Fond = document.getElementById("fond");
 var body = document.getElementsByTagName("body")[0];
 
+var nbCanons = 3;
+
+
+
+
 function main(){
 	var i = 0;
-	var canon = create_element('canon', 80, 80, 50, 500);
-	var canon2 = create_element('canon', 80, 80, 250, 350);
+	var canons = [];
 	var score = create_element('score', 80, 80, Fond.width/2 - 40, 15);
+	var espace = (ecran_height - 80)/3;
+
+	for (var i = 0; i < nbCanons; i++) {
+		canons.push(create_element('canon', 80, 80, randint(0,ecran_width-80), 80 + i*espace));
+	}
+	//var anim = rotate(canon, 1, 50);
+	//translate(canon, getXY(canon), 0, 1, 10, 10);
 } 
 
 function create_element(nature, width, height, x, y){
@@ -59,7 +70,7 @@ function draw_score(ctx, width, height, score){
 	ctx.fillText(""+score, width/2, height/2);
 }
 
-function rotation(c, angle){
+function rotationCanon(c, angle){
 	var ctx = c.getContext('2d');
 	ctx.clearRect(0, 0, c.width, c.height);
 	ctx.translate(c.width/2, c.height/2);
@@ -68,12 +79,81 @@ function rotation(c, angle){
 	draw_canon(ctx, c.width, c.height);
 }
 
-function rotate(c, angle, vitesse){
+function rotate(c, deltaAngle, vitesse){
 	var anim = setInterval(function(){
-		rotation(c, angle);
+		rotationCanon(c, deltaAngle);
 	}, vitesse);
 
 	return anim
+}
+
+function translation(elt, direction, delta){
+	var x = getXY(elt)[0], y = getXY(elt)[1];
+	x = x + delta*Math.sin(radian(direction));
+	y = y - delta*Math.cos(radian(direction));
+	positionne(elt, x, y);
+}
+
+function translate(elt, init, direction, delta, limite, vitesse){
+	var anim = setInterval(function(){
+		translation(elt, direction, delta);
+		if(distance(init, getXY(elt))>limite){
+			//TODO
+			clearInterval(anim);
+		}
+	}, vitesse);
+}
+
+function getXY(elt){
+	return [getValue(elt.style.left), parseInt(elt.style.top)] ;
+}
+
+function redimensionne(elt, width , height){
+	//donne width et height a l'element elt
+	elt.style.width = width+"px";
+	elt.style.height = height+"px";
+}
+
+function positionne(elt, x, y){
+	elt.style.position = 'absolute';
+	elt.style.top = y+"px";
+	elt.style.left = x+"px";
+}
+
+function hide(elt){
+	elt.style.display = 'none';
+}
+
+function show(elt){
+	elt.style.display = 'block';
+}
+
+function radian(degre){
+	return (Math.PI*degre)/180;
+}
+
+function distance(a, b){
+	return Math.sqrt((a[0]-b[0])^2 + (a[1]-b[1])^2);
+}
+
+function getValue(text){
+	var nbr = "", digit;
+	for (var i = 0; i < text.length; i++) {
+		digit = text[i];
+		if(digit == 'p'){
+			break;
+		}
+		else{
+			nbr+=digit;
+		}
+	}
+
+	return parseInt(nbr);
+}
+
+function randint(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 main();
