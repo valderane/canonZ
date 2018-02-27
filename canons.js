70928d8524e237109a8a8ea45d8033bba73771fd
valderane <valderane@yahoo.fr>
2018-02-27 01:54:24 +0100

    la balle fonctionne à peu près

--- canons.js
+++ canons.js
@@ -5,20 +5,39 @@ var Fond = document.getElementById("fond");
 var body = document.getElementsByTagName("body")[0];
 
 var nbCanons = 3;
+var canonWidth = 80;
+var espaceAvantCanon = 90;
 
+//creation de la balle
+var balle = create_element('balle', canonWidth, canonWidth, 0, 0);
+
+var angleGlobal = 0;
+var score = 0;
+var eps = 1;
+
+var canons = [];
+var score_contenair;
+var animRotation;
 
 
 
 function main(){
 	var i = 0;
-	var canons = [];
-	var score = create_element('score', 80, 80, Fond.width/2 - 40, 15);
-	var espace = (ecran_height - 80)/3;
+	score_contenair = create_element('score', canonWidth, canonWidth, Fond.width/2 - 40, 15);
+	var espace = (ecran_height - espaceAvantCanon)/3;
+
+	hide(balle);
 
 	for (var i = 0; i < nbCanons; i++) {
-		canons.push(create_element('canon', 80, 80, randint(0,ecran_width-80), 80 + i*espace));
+		canons.push(create_element('canon', canonWidth, canonWidth, randint(0,Fond.width-80), espaceAvantCanon + i*espace));
 	}
-	//var anim = rotate(canon, 1, 50);
+
+	animRotation = rotate(canons[nbCanons - 1], 1, 40);
+
+	Fond.addEventListener('click', function(){
+		//TODO
+		translateBalle(2,eps,10);
+	});
 	//translate(canon, getXY(canon), 0, 1, 10, 10);
 } 
 
@@ -51,7 +70,7 @@ function draw_canon(ctx, width, height){
 	// draw a canon on a context ctx
 	ctx.beginPath();
 	ctx.arc(width/2, height/2, 20, 0, 2*Math.PI);
-	ctx.fillStyle = "#BACADA";
+	ctx.fillStyle = "#FFFFFF";
 	ctx.fill();
 	ctx.fillRect(width/2-10, height/2-30, 20, 20);
 }
@@ -65,7 +84,7 @@ function draw_balle(ctx, width, height){
 
 function draw_score(ctx, width, height, score){
 	ctx.textAlign = "center";
-	ctx.fillStyle = "#000000";
+	ctx.fillStyle = "#FFFFFF";
 	ctx.font="50px Arial";
 	ctx.fillText(""+score, width/2, height/2);
 }
@@ -82,6 +101,8 @@ function rotationCanon(c, angle){
 function rotate(c, deltaAngle, vitesse){
 	var anim = setInterval(function(){
 		rotationCanon(c, deltaAngle);
+		angleGlobal = (angleGlobal+deltaAngle)%360;
+
 	}, vitesse);
 
 	return anim
@@ -101,6 +122,48 @@ function translate(elt, init, direction, delta, limite, vitesse){
 			//TODO
 			clearInterval(anim);
 		}
+	}, vitesse);
+}
+
+function translateBalle(delta, eps, vitesse){
+	var coordsInit = getXY(canons[nbCanons-1]);
+	var animBalle;
+	var c;
+	var coordsCanons = [];
+	clearInterval(animRotation);
+	var angle = angleGlobal;
+	show(balle);
+	for (var i = 0; i < canons.length ; i++) {
+		c = getXY(canons[i]);
+		coordsCanons.push([c[0] + canonWidth/2, c[1] + canonWidth/2]);
+	}
+
+	positionne(balle, coordsInit[0], coordsInit[1]);
+	animBalle = setInterval(function(){
+		translation(balle, angle, delta);
+		coordsInit = getXY(balle);
+		if( coordsInit[0]<0 || coordsInit[0]>Fond.width || coordsInit[1]<0 || coordsInit[1]>Fond.height ){
+			clearInterval(animBalle);
+		}
+		else{
+			for (var i = canons.length - 2; i >= 0; i--) {
+				if(distance(coordsCanons[i], [getXY(balle)[0]+canonWidth/2, getXY(balle)[1]+canonWidth/2])<eps){
+					hide(balle);
+					clearInterval(animBalle);
+					if(i == 0){
+						score+=2;
+						draw_score(score_contenair.getContext('2d'), score_contenair.width, score_contenair.height, score);
+
+					}
+					else{
+						score+=1;
+						draw_score(score_contenair.getContext('2d'), score_contenair.width, score_contenair.height, score);
+					}
+					//TODO
+				}
+			}
+		}
+		
 	}, vitesse);
 }
 
@@ -154,6 +217,10 @@ function getValue(text){
 function randint(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function remove(elt){
+	body.removeChild(elt);
 }
 
 main();
