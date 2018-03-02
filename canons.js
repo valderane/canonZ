1d511dc70636b74134452fac0bb65d1289b93108
valderane <valderane@yahoo.fr>
2018-03-02 01:13:35 +0100

    gameplay de base en place

--- canons.js
+++ canons.js
@@ -6,23 +6,26 @@ var body = document.getElementsByTagName("body")[0];
 
 var nbCanons = 3;
 var canonWidth = 80;
-var canonsCoords = [];
-var vitesseRotationCanons = 10;
+var vitesseRotationCanons = 20;
 var espaceAvantCanon = 90;
 var espace = parseInt((ecran_height - espaceAvantCanon)/3)
 
 //creation de la balle
 var balleWidth = 20;
 var balle = {elt:create_element('balle', balleWidth, balleWidth, 0, 0), x:0, y:0};
-var vitesseBalle = 1;
+var vitesseBalle = 10;
 
 var angleGlobal = 0;
 var score = 0;
-var eps = 50;
+var eps = 30;
 
 var canons = [];
 var score_contenair;
-var animRotation;
+var animRotation = [];
+
+var once = false;
+
+var peutCliquer = true;
 
 
 
@@ -31,20 +34,28 @@ function main(){
 	score_contenair = create_element('score', canonWidth, canonWidth, Fond.width/2 - 40, 15);
 	espace = parseInt((ecran_height - espaceAvantCanon)/3), x = 0;//TODO rentre les coords flotantes
 
-	//hide(balle);
+	hide(balle.elt);
 
 	for (var i = 0; i < nbCanons; i++) {
 		x = randint(0,Fond.width - canonWidth);
-		canons.push(create_element('canon', canonWidth, canonWidth, x, espaceAvantCanon + i*espace));
-		canonsCoords.push([x, espaceAvantCanon + i*espace]);
+		canons.push({elt:create_element('canon', canonWidth, canonWidth, x, espaceAvantCanon + i*espace), x:x, y:espaceAvantCanon + i*espace});
+		//canonsCoords.push([x, espaceAvantCanon + i*espace]);
 
 	}
-	console.log(canonsCoords);
-	animRotation = rotate(canons[nbCanons - 1], 1, vitesseRotationCanons);
-
+	animRotation.push(rotate(canons[nbCanons - 1].elt, Math.sign(canons[nbCanons-2].x - canons[nbCanons-1].x), 1, vitesseRotationCanons));
+	console.log("animRotation"+animRotation);
 	Fond.addEventListener('click', function(){
 		//TODO
-		translateBalle(2,eps,vitesseBalle);
+		if(peutCliquer){
+			peutCliquer = false;
+			clearInterval(animRotation[0]);
+			animRotation.splice(0, 1);
+			console.log(animRotation);
+			translateBalle(2,eps,vitesseBalle);
+			translate(canons[nbCanons-1].elt, getXY(canons[nbCanons-1].elt), 180 + angleGlobal, 2, 10, 10);
+		
+		}
+		
 	});
 	//translate(canon, getXY(canon), 0, 1, 10, 10);
 } 
@@ -82,9 +93,7 @@ function draw_canon(ctx, width, height){
 	ctx.fill();
 	ctx.fillRect(width/2-10, height/2-30, 20, 20);
 	ctx.beginPath();
-	ctx.arc(width/2, height/2, 1, 0, 2*Math.PI);
-	ctx.fillStyle = "#0000FF";
-	ctx.fill();
+	ctx.arc(width/2, height/2, eps, 0, 2*Math.PI);
 	ctx.strokeStyle="red";
 	ctx.stroke();
 }
@@ -94,12 +103,6 @@ function draw_balle(ctx, width, height){
 	ctx.arc(width/2, height/2, 10, 0, 2*Math.PI);
 	ctx.fillStyle = "#AACCDE";
 	ctx.fill();
-	ctx.beginPath();
-	ctx.arc(width/2, height/2, 1, 0, 2*Math.PI);
-	ctx.fillStyle = "#FF0000";
-	ctx.fill();
-	ctx.strokeStyle="red";
-	ctx.stroke();
 }
 
 function draw_score(ctx, width, height, score){
@@ -120,10 +123,11 @@ function rotationCanon(c, angle){
 	draw_canon(ctx, c.width, c.height);
 }
 
-function rotate(c, deltaAngle, vitesse){
+function rotate(c, sens, deltaAngle, vitesse){
 	var anim = setInterval(function(){
-		rotationCanon(c, deltaAngle);
-		angleGlobal = (angleGlobal+deltaAngle)%360;
+		rotationCanon(c, sens*deltaAngle);
+		
+		angleGlobal = (angleGlobal+sens*deltaAngle)%360;
 
 	}, vitesse);
 
@@ -147,6 +151,32 @@ function translate(elt, init, direction, delta, limite, vitesse){
 	}, vitesse);
 }
 
+
+function translateCanon(canon, indiceCanon, delta, limite, vitesse){
+	var y = canons[indiceCanon].y, x = canons[indiceCanon].x;
+	var anim = setInterval(function(){
+		y += delta;
+		positionne(canon,x, y);
+		if(y>limite){
+			//TODO
+			y = limite;
+			positionne(canon, x, y);
+			canons[indiceCanon].y = y;
+			peutCliquer = true;
+			angleGlobal = 0;
+			if(!once){
+				animRotation.push(rotate(canons[nbCanons-1].elt, Math.sign(canons[nbCanons-2].x - canons[nbCanons-1].x) ,1 , vitesseRotationCanons));
+				once = true;
+			}
+			clearInterval(anim);
+			for (var i = 0; i < nbCanons - 1; i++) {
+				show(canons[i].elt);
+			}
+			
+		}
+	}, vitesse);
+}
+
 function translationBalle(direction, delta){
 	balle.x += delta*Math.sin(radian(direction));
 	balle.y -= delta*Math.cos(radian(direction));
@@ -154,11 +184,11 @@ function translationBalle(direction, delta){
 }
 
 function translateBalle(delta, eps, vitesse){
-	var coordsInit = canonsCoords[nbCanons-1];
+	var coordsInit = [canons[nbCanons-1].x, canons[nbCanons-1].y];
 	var animBalle;
 	var c;
-	clearInterval(animRotation);
 	var angle = angleGlobal;
+	once = false;
 	
 
 
@@ -181,49 +211,50 @@ function translateBalle(delta, eps, vitesse){
 		}
 		else{
 			for (var i = canons.length - 2; i >= 0; i--) {
-				d = distance([canonsCoords[i][0] + canonWidth/2, canonsCoords[i][1] + canonWidth/2], [balle.x+balleWidth/2, balle.y+balleWidth/2]);
-				console.log(d);
+				d = distance([canons[i].x + canonWidth/2, canons[i].y + canonWidth/2], [balle.x+balleWidth/2, balle.y+balleWidth/2]);
 				if(d<=eps){
-					//hide(balle);
+					hide(balle.elt);
 					clearInterval(animBalle);
 					if(i == 0){
 						score+=2;
 						draw_score(score_contenair.getContext('2d'), score_contenair.width, score_contenair.height, score);
 
 						for (var j = 1; j < canons.length; j++) {
-							remove(canons[j]); 
+							remove(canons[j].elt); 
 						}
-						translate(canons[0], canonsCoords[0], 180, 2, 2*espace, 10);
+						
 						canons[2] = canons[0];
-
-						canonsCoords[2][0] = canonsCoords[0][0];
+						translateCanon(canons[2].elt, 2, 2, espaceAvantCanon + 2*espace, 10);
+						
+						//canons[2].y = espaceAvantCanon + 2*espace; 
 
 						for (var j = 0; j < nbCanons-1; j++) {
-							x = randint(0, Fond.width);
-							canons[j] = create_element('canon', canonWidth, canonWidth, x, espaceAvantCanon + j*espace); 
-							canonsCoords[j][0] = x;
+							x = randint(0, Fond.width - canonWidth );
+							canons[j] = {elt:create_element('canon', canonWidth, canonWidth, x, espaceAvantCanon + j*espace), x:x, y:espaceAvantCanon + j*espace}; 
+							hide(canons[j].elt);
+							console.log("canon "+j+" [ "+canons[j].x+","+canons[j].y+" ]");
 						}
+
 					}
 					else{
-						x = randint(0, Fond.width);
+						x = randint(0, Fond.width - canonWidth);
 						score+=1;
 						draw_score(score_contenair.getContext('2d'), score_contenair.width, score_contenair.height, score);
 
-						remove(canons[2]);
-
-						var a = canonsCoords[0], b = canonsCoords[1];
-
-						translate(canons[0], a, 180, 2, espace , 10);
-						translate(canons[1], b, 180, 2, espace, 10);
+						remove(canons[2].elt);
 
 						canons[2] = canons[1];
 						canons[1] = canons[0];
 
-						canonsCoords[2][0] = canonsCoords[1][0];
-						canonsCoords[1][0] = canonsCoords[0][0];
-						canons[0] = create_element('canon', canonWidth, canonWidth, x, espaceAvantCanon);
-						canonsCoords[0][0] = x;
+						translateCanon(canons[2].elt, 2, 2, espaceAvantCanon +  2*espace , 10);
+						translateCanon(canons[1].elt, 1, 2, espaceAvantCanon +  espace, 10);
 
+						//canonsCoords[2][0] = canonsCoords[1][0];
+						//canonsCoords[1][0] = canonsCoords[0][0];
+
+						canons[0] = {elt:create_element('canon', canonWidth, canonWidth, x, espaceAvantCanon), x:x, y:espaceAvantCanon};
+						hide(canons[0].elt);
+						console.log("canon "+0+" [ "+canons[0].x+","+canons[0].y+" ]");
 
 					}
 					/*
@@ -251,9 +282,7 @@ function translateBalle(delta, eps, vitesse){
 						canonsCoords[j][0] = x;
 					} */
 
-					console.log(canonsCoords);
-
-					animRotation = rotate(canons[nbCanons-1], 1 , vitesseRotationCanons);
+					break;
 
 					//TODO
 				}
